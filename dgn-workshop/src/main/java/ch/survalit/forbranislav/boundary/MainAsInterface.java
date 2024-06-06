bbdfb9940447890880b0e04d2adb09d34cd34b7b
Christian Frei <christian.frei@gmail.com>
2024-06-06 14:40:39 +0200

    main as interface

---
+++ dgn-workshop/src/main/java/ch/survalit/forbranislav/boundary/MainAsInterface.java
@@ -0,0 +1,12 @@
+package ch.survalit.forbranislav.boundary;
+
+public interface MainAsInterface {
+
+    static void main(String[] args) {
+        System.out.println("""
+                I can run a main method in an interface and save the public word.
+                Does it compile? sure!
+                Is it morally wrong? maybe...  ;-)
+                """);
+    }
+}
